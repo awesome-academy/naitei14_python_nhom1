52ada336b4de04e80d5cded390dc532a48fc9e98
letuboihoang <le.tu.boi.hoang@sun-asterisk.com>
2025-12-15 21:04:05 +0700

    feat/i18n + fix favorite
    Fix review comments

--- PitchManager/urls.py
+++ PitchManager/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('django.contrib.auth.urls')),
